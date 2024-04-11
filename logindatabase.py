0d653e9ce0e6340a7f8865882846cfc982c66a4e
JATIN <119930945+Jatin00001@users.noreply.github.com>
2024-04-11 09:35:48 +0000

    working # -------------------------------------------------------------------------------------------------------------------------------why we convert into dict -------------------------------------/     # for row in result:     #   print(row)     # conn.execute(     #     text(     #       "INSERT INTO userdatalogin (username, password) VALUES ('lucky', '123456')"     #     ))     # conn.commit()     # for row in result:     #   print(row)     # print(result.all()) #  type --->class list     # print(type(result)) # type ---> class 'sqlalchemy.engine.cursor.CursorResult'      #now we check whats on first index in row and check its class its      # First_postion = result.all[0]     # print("First pos data class : " , type(First_postion)) #<class 'sqlalchemy.engine.cursor.CursorResult'>'method' object is not subscriptable

--- logindatabase.py
+++ logindatabase.py
@@ -1,14 +1,20 @@
 import sqlalchemy
 from sqlalchemy import create_engine, text
 
-engine = create_engine(
-    "mysql+pymysql://root:fXIllItNLMVVSzRwHPFWGzJfZGNlxykr@monorail.proxy.rlwy.net:51990/railway"
-)
+db_connection_string = "mysql+pymysql://root:fXIllItNLMVVSzRwHPFWGzJfZGNlxykr@monorail.proxy.rlwy.net:51990/railway"
+# connect_args = {'ssl': {'cert': '/path/to/certificate.pem'}}
+engine = create_engine(db_connection_string)
 
 try:
   with engine.connect() as conn:
-    print("Connected to the database -- >", conn)
-    result = conn.execute(text("SELECT * FROM userdatalogin"))
+    # print("Connected to the database -- >", conn)
+    result = conn.execute(text("SELECT username, password FROM userdatalogin"))
+    for row in result:
+      print(f"username: {row.username}  passoword: {row.password}")
+
+
+
+# -------------------------------------------------------------------------------------------------------------------------------why we convert into dict -------------------------------------/
     # for row in result:
     #   print(row)
     # conn.execute(
@@ -16,8 +22,14 @@ try:
     #       "INSERT INTO userdatalogin (username, password) VALUES ('lucky', '123456')"
     #     ))
     # conn.commit()
-    for row in result:
-      print(row)
+    # for row in result:
+    #   print(row)
+    # print(result.all()) #  type --->class list
+    # print(type(result)) # type ---> class 'sqlalchemy.engine.cursor.CursorResult'
 
+    #now we check whats on first index in row and check its class its 
+    # First_postion = result.all[0]
+    # print("First pos data class : " , type(First_postion)) #<class 'sqlalchemy.engine.cursor.CursorResult'>'method' object is not subscriptable
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 except Exception as e:
   print(e)
