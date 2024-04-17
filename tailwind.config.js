dc4892f480e84173f7083340f4b0b394a7ff8a72
JATIN <119930945+Jatin00001@users.noreply.github.com>
2024-04-17 16:42:36 +0000

    fix tailwind error now all working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["./templates/*"],
+  content: ["./templates/*", "./templates/users/*"],
   theme: {
     extend: {
       colors: {
